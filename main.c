e8272135d2f5e68a3d772d578a3a4c0c62ae9193
Felipe Torrezan <felipe.torrezan@iar.com>
2021-11-18 15:28:19 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#if !defined (NDEBUG)
+# include <stdlib.h>
+#endif
+
+unsigned int x = 0;
+
+int main()
+{
+  for (; x < 10; x++)
+  {
+    printf("Hello world %u\n", x);
+  }
+  while(1);
+}
+
+#if !defined(NDEBUG)
+__root void _softwareAbort()
+{
+  abort();
+}
+#endif
